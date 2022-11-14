bf06468ec426fe33d36a6ddf1d9ac8cbb7fa4b57
Michprac <michal.poczta135@gmail.com>
2022-11-14 17:10:31 +0100

    Added prompts for guessing word in the file words.py, the ability to use prompts was added

--- main.py
+++ main.py
@@ -2,29 +2,34 @@
 
 import random
 import string
-from words import words
+from words import words, prompts
 
 
-def get_valid_word(list_of_words):  # choising word without any symbol like space or hyphen
+def get_valid_word(list_of_words, list_of_prompts):  # choising word without any symbol like space or hyphen
     word = random.choice(list_of_words)
     while '-' in word or ' ' in word:
-        word = random.choice(list_of_words)
-    return word
+        word = random.choice(list_of_words)   #output word
+    index_of_word = list_of_words.index(word);   #getting index of chosen word in "words" list
+    prompts_for_game = list_of_prompts[index_of_word]   #output prompts
+    return word, prompts_for_game
 
 def hangman():
-    word_for_game = get_valid_word(words).upper()
+    chosen_word, prompts_for_word = get_valid_word(words, prompts)
+    word_for_game = chosen_word.upper()
     letters_of_word = set(word_for_game)
     alphabet = set(string.ascii_uppercase)  # list of letters for guessing
     used_letters = set()
     lives = 5   # you have 5 attempts for make mistake
+    prompts_counter = 0
 
     while len(letters_of_word) > 0 and lives > 0:
+        print("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~")
         print("You have {}".format(lives), "lives and you have used these letters: ", ' '.join(used_letters))
         current_word = [letter if letter in used_letters else '_' for letter in word_for_game]
         print("Current word:", ' '.join(current_word))
 
-        guess = input("Please, choose a letter: ").upper()   # user is choosing a letter. It is uppercase, because we
-        if guess in (alphabet - used_letters):               # work with this font
+        guess = input("Please, choose a letter (for a prompt choose '?', -1 leave): ").upper()   # user is choosing a letter.
+        if guess in (alphabet - used_letters):               # It is uppercase, because we work with this font
             used_letters.add(guess)
             if guess in letters_of_word:
                 letters_of_word.remove(guess)   # removing correct letter from list of target word
@@ -32,6 +37,14 @@ def hangman():
                 lives = lives - 1
         elif guess in used_letters:
             print("\n !!!You have already used this letter. Please, choose another!!! \n")
+        elif guess == '?':
+            if prompts_counter < 3:
+                print(f"???? Your prompt: {prompts_for_word[prompts_counter].upper()} ????")
+                print("-1 live")
+                lives = lives - 1
+                prompts_counter = prompts_counter + 1
+            else:
+                print("???? Sorry, there are no more prompts for the word ????")
         else:
             print("There is no such kind of symbol in this list")
     if lives == 0:
--- words.py
+++ words.py
@@ -1,1 +1,10 @@
-words = ['sun', 'ice-cream', 'green apple', 'machine', 'house', 'garden']
+words = ['sun', 'ice-cream', 'green apple', 'car', 'house', 'garden']
+
+prompts_for_sun = ["It's a huge thing", "We get light from it", "It is in the sky"];
+prompts_for_ice_cream = ["It's cold", "It is can be with fruits", " It is sweet"];
+prompts_for_green_apple = ["It is a fruit", "It grows in the garden on a tree", "Newton..."];
+prompts_for_car = ["It usually has 4 doors", "Helps move faster", "You can meet it on the road"];
+prompts_for_house = ["It has a window and doors", "It is a building", "There can be a fireplace"];
+prompts_for_garden = ["Many trees grow here", "You can pick vegetables and fruits here", "It is can be near your house"];
+
+prompts = [prompts_for_sun, prompts_for_ice_cream, prompts_for_green_apple, prompts_for_car, prompts_for_house, prompts_for_garden];
